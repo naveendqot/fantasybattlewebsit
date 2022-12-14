ec5d5d3357072cc8ca322733e9d17e5e69cb101f
naveendqot <naveen@dqotsolutions.com>
2022-12-14 19:02:30 +0530

    updatE

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
-URL::forceScheme('https');
+//URL::forceScheme('https');
 
 /*
 |--------------------------------------------------------------------------
